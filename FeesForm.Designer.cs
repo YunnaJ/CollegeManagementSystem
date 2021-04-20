e63b630fd85e9f89f656be59cf4d06fa59442bfa
Sukhmanpreet <Sukhmanpreet.Singh@MyGeorgian.ca>
2021-04-20 18:15:48 -0400

    fees form added

--- FeesForm.Designer.cs
+++ FeesForm.Designer.cs
@@ -29,11 +29,121 @@ namespace CollegeManagementSystem
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.studentIDtext = new System.Windows.Forms.TextBox();
+            this.studentNameText = new System.Windows.Forms.TextBox();
+            this.feesTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Script MT Bold", 72F, ((System.Drawing.FontStyle) ((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(124, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(487, 125);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "$ Fees";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label2.Location = new System.Drawing.Point(70, 216);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Student ID";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label3.Location = new System.Drawing.Point(70, 296);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(168, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Student Name:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label4.Location = new System.Drawing.Point(70, 370);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(168, 29);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Fees:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // studentIDtext
+            // 
+            this.studentIDtext.Location = new System.Drawing.Point(424, 219);
+            this.studentIDtext.Name = "studentIDtext";
+            this.studentIDtext.Size = new System.Drawing.Size(202, 22);
+            this.studentIDtext.TabIndex = 4;
+            this.studentIDtext.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // studentNameText
+            // 
+            this.studentNameText.Location = new System.Drawing.Point(424, 296);
+            this.studentNameText.Name = "studentNameText";
+            this.studentNameText.Size = new System.Drawing.Size(202, 22);
+            this.studentNameText.TabIndex = 5;
+            // 
+            // feesTextBox
+            // 
+            this.feesTextBox.Location = new System.Drawing.Point(424, 370);
+            this.feesTextBox.Name = "feesTextBox";
+            this.feesTextBox.Size = new System.Drawing.Size(202, 22);
+            this.feesTextBox.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.button1.Location = new System.Drawing.Point(324, 492);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 25);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FeesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(770, 588);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.feesTextBox);
+            this.Controls.Add(this.studentNameText);
+            this.Controls.Add(this.studentIDtext);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FeesForm";
             this.Text = "FeesForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
+
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.TextBox studentIDtext;
+        private System.Windows.Forms.TextBox studentNameText;
+        private System.Windows.Forms.TextBox feesTextBox;
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.Label label1;
 
         #endregion
     }
